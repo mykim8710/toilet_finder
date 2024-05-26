0245c3c8c2967d704dbff2ba5b24c2c5aabd0182
mykim8710 <mykim8710@gmail.com>
2024-05-26 18:11:58 +0900

    [add] spring boot 프로젝트 생성 및 초기 세팅 - build.gradle(추후 부가적으로 필요한 의존성은 그때 그때 추가예정) - application.yml

---
+++ src/main/java/com/service/toiletfinder/ToiletFinderApplication.java
@@ -0,0 +1,13 @@
+package com.service.toiletfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToiletFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/toiletfinder/ToiletFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.toiletfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
